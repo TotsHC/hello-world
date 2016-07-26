8787b9b42833ee70dc44e6a3a70b5f98cfa20796
Drew Morriss <dmorriss@shortridgeacademy.com>
2016-07-26 11:21:21 -0400

    Added math stuff

---
+++ PyMath.py
@@ -0,0 +1,18 @@
+print 8 * 3.57
+print 10 * 365
+print 20 + 3650
+print 3 * 52
+print 3670 - 156
+print (5 + 30) * 20
+print ((5 + 30) * 20) / 10
+
+things = "stuff"
+print things
+
+blue_coins = 10
+red_coins = 15
+green_coins = 5
+
+print blue_coins * red_coins + green_coins - (red_coins / blue_coins)
+
+print "You cant\'t take all of they\'re red coins!"
