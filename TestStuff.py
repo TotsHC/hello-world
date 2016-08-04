c40233cb123aef345ba6bd150ab764eaa9f6b078
Drew Morriss <dmorriss@shortridgeacademy.com>
2016-08-04 11:29:05 -0400

    Things counter

--- TestStuff.py
+++ TestStuff.py
@@ -1,2 +1,9 @@
-for x in range(1, 100):
-    print ("There are %s Memes" % x)
+things = raw_input("Input a number of things: ")
+print "You input %s Things" % (things)
+things = int(things)
+if things < 10:
+    print "There are less than 10 things"
+elif things > 10:
+    print "There are more than 10 things"
+else:
+    print "There are 10 things"
