cf15428eb2c5b4ad4efb73d49740bc3137fbdf45
Drew Morriss <dmorriss@shortridgeacademy.com>
2016-09-30 14:46:51 -0400

    PyMath listing numbers that add to you inputed number

--- PyMath.py
+++ PyMath.py
@@ -1,18 +1,11 @@
-print 8 * 3.57
-print 10 * 365
-print 20 + 3650
-print 3 * 52
-print 3670 - 156
-print (5 + 30) * 20
-print ((5 + 30) * 20) / 10
-
-things = "stuff"
-print things
-
-blue_coins = 10
-red_coins = 15
-green_coins = 5
-
-print blue_coins * red_coins + green_coins - (red_coins / blue_coins)
-
-print "You cant\'t take all of they\'re red coins!"
+def factors(b):
+    for i in range(1, b+1):
+        if b % i == 0:
+            print(i)
+if __name__ == '__main__':
+    b = input('Your Number Please: ')
+    b = float(b)
+    if b > 0 and b.is_integer():
+        factors(int(b))
+    else:
+        print('Please enter a positive integer')
