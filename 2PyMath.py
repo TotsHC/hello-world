36f278069b352255debcd3907440e3017bb3f2ae
Drew Morriss <dmorriss@shortridgeacademy.com>
2016-10-07 15:20:16 -0400

    More pymath stuffs

--- 2PyMath.py
+++ 2PyMath.py
@@ -18,3 +18,18 @@ def draw_trajectory(u, theta):
     t_flight = 2*u*math.sin(theta)/g
 
     intervals = frange(0, t_flight, 0.001)
+    x = []
+    y = []
+    for t in intervals:
+        x.append(u*math.cos(theta)*t)
+        y.append(u*math.sin(theta)*t - 0.5*g*t*t)
+    draw_graph(x, y)
+if __name__ == '__main__':
+    try:
+        u = float(input('Enter the initial velocity (m/s): '))
+        theta = float(input('Enter the angle of projection (degrees): '))
+    except ValueError:
+        print('You entered an invalid input')
+    else:
+        draw_trajectory(u, theta)
+        plt.show()
