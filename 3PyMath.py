de34a1477c5ec9119dca7b7c3372b5ea4ec07f2b
Drew Morriss <dmorriss@shortridgeacademy.com>
2016-10-17 14:54:00 -0400

    fixed some number input system

--- 3PyMath.py
+++ 3PyMath.py
@@ -4,6 +4,12 @@ def calculate_mode(numbers):
     mode = c.most_common(1)
     return mode[0][0]
 if __name__=='__main__':
-    scores = raw_input("Input Scores: ")
-    mode = calculate_mode(scores)
+    number1 = raw_input("Input first number: ")
+    number2 = raw_input("Input second number: ")
+    number3 = raw_input("Input third number: ")
+    number4 = raw_input("Input fourth number: ")
+    number5 = raw_input("Input fith number: ")
+    scores = (int(number1), int(number2), int(number3), int(number4), int(number5))
+    scores2 = scores
+    mode = calculate_mode(scores2)
     print('The mode of the list of numbers is: {0}'.format(mode))
