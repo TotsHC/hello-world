14fb5911cc2d537a1ed4eff83521f44560e5b534
Drew Morriss <dmorriss@shortridgeacademy.com>
2016-10-20 11:14:59 -0400

    Made random number dice roll, until certain number reached

---
+++ 5PyMath.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+import random
+target_score = 50
+def roll():
+    return random.randint(1, 10)
+if __name__ == '__main__':
+    score = 0
+    num_rolls = 0
+    while score < target_score:
+        die_roll = roll()
+        num_rolls += 1
+        print('Rolled: {0}'.format(die_roll))
+        score += die_roll
+    print('Score of {0} reached in {1} rolls'.format(score, num_rolls))
